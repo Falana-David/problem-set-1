fbf807dc235cb5c1dbbd36b6f58c0a2c5b7189b4
David Falana <falanadavid@gmail.com>
2018-09-18 11:44:34 -0400

    Final Commit For Real

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -1,11 +1,3 @@
-/**
- * INSTRUCTIONS.
- * 
- * Problem Set 1 will test your skills of printing to the console. You'll
- * be asked to print various text, shapes, and figures, all of which is
- * explained (and demonstrated) in detail in the README file.
- */
-
 public class ProblemSet1 {	
 	
 	/**
@@ -15,15 +7,42 @@ public class ProblemSet1 {
 	
 	public static void main(String[] args) {
 		System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
-		System.out.println(" ##")/n;
-		System.out.println(" #");
-		System.out.println("#");
-		System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
-		
+				System.out.println("   ##    ######  ######## ########"); 
+				System.out.println("  #  #   #     # #        "); 
+				System.out.println(" #    #  #     # #        #");  
+				System.out.println("######## ######  #        ########");  
+				System.out.println("#      # #       #               #"); 
+				System.out.println("#      # #       #               #"); 
+				System.out.println("#      # #       ######## ########");
+				System.out.println("");
+				
+				System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
+						System.out.println(    "      /\\     /\\     /\\");
+						System.out.println("     /  \\   /  \\   /  \\");  
+						System.out.println("    /    \\ /    \\ /    \\");  
+						System.out.println("    \\    / \\    / \\    /");
+						System.out.println("     \\  /   \\  /   \\  /");
+						System.out.println("      \\/     \\/     \\/"); 
+						System.out.println("");
+
 		System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
+				System.out.println("   \\\\\\\\\\");  
+				System.out.println("   \"\"\"\"\"");  
+				System.out.println("(|  0 0  |)"); 
+				System.out.println(" |   ^   |"); 
+				System.out.println(" |  '-'  |"); 
+				System.out.println("  +-----+");
+				System.out.println("");
 		
 		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
+		System.out.println( "H E L L O"); 
+		System.out.println("E       A");
+		System.out.println("L       P"); 
+		System.out.println("L       C");  
+		System.out.println("O A P C S");
+		System.out.println("");
 		
 		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
+		System.out.println("\"The answer's quite simple,\" the professor said. \"Just think outside the box!\"");
 	}
 }
